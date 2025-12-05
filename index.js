47ec8645cf57af28e24d283f4d32c79e93544b95
Aida-Zborovska <izograf.zinaida@gmail.com>
2025-12-05 18:36:55 +0000

    Deploying to gh-pages from @ Aida-Zborovska/blended-js-final-project@b8672a0028264e750542f68c439fd2baefd391f3 🚀

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import"./assets/styles-DAE99nJD.js";import{a as e}from"./assets/vendor-2s9xPmg-.js";const n="https://dummyjson.com",r={CATEGORIES:"products/category-list"};e.defaults.baseURL=n;async function a(){return(await e.get(r.CATEGORIES)).data}const c={categoriesList:document.querySelector("ul.categories")};function i(t){const o=["All",...t].map(s=>`<li class="categories__item">
-      <button class="categories__btn" type="button">${s}</button>
-    </li>`).join("");c.categoriesList.innerHTML=o}async function u(){try{const t=await a();i(t)}catch(t){console.error(t)}}document.addEventListener("DOMContentLoaded",u);
+import"./assets/styles-DAE99nJD.js";import{a as r}from"./assets/vendor-2s9xPmg-.js";const l="https://dummyjson.com",c={CATEGORIES:"products/category-list",PRODUCTS:"products"},_=12;r.defaults.baseURL=l;async function g(){return(await r.get(c.CATEGORIES)).data}async function m(t){const s=(t-1)*12;return(await r.get(`${c.PRODUCTS}?limit=${_}&skip=${s}`)).data}const a={categoriesList:document.querySelector("ul.categories"),productsList:document.querySelector("ul.products")};function $(t){const e=["All",...t].map(o=>`<li class="categories__item">
+      <button class="categories__btn" type="button">${o}</button>
+    </li>`).join("");a.categoriesList.innerHTML=e}function y({products:t}){const s=t.map(({id:e,thumbnail:o,description:n,title:i,brand:p,category:u,price:d})=>`<li class="products__item" data-id="${e}">
+    <img class="products__image" src="${o}" alt="${n}"/>
+    <p class="products__title">${i}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:</span></p>${p}
+    <p class="products__category">Category: ${u}</p>
+    <p class="products__price">Price: ${d}$</p>
+ </li>`).join("");a.productsList.innerHTML=s}async function L(){try{const t=await g();$(t)}catch(t){console.error(`Get categories error: ${t}`)}try{const t=await m(1);y(t)}catch(t){console.error(`Get products error: ${t}`)}}document.addEventListener("DOMContentLoaded",L);
 //# sourceMappingURL=index.js.map
