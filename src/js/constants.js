8ea3082fbd73fc0dcf58e2c0096ba0c907bb0393
Aїda <izograf.zinaida@gmail.com>
2025-12-07 22:51:40 +0200

    feature/form-handler

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,6 +3,7 @@ export const API_ENDPOINTS = {
   CATEGORIES: 'products/category-list',
   PRODUCTS: 'products',
   CATEGORY: 'products/category/',
+  SEARCH: 'products/search',
 };
 export const PER_PAGE = 12;
 export const STATE = {
